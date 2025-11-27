da10cdbdc17e1fac5c286695d5ee4987ee9529ef
uzicorn <zenini.salim@gmail.com>
2025-11-27 19:05:03 +0100

    local uncommitted function

--- git_report.py
+++ git_report.py
@@ -2,11 +2,17 @@ import subprocess
 import os
 
 source = "/home/uzicorn/code/is_git/test"
-uncommited_command = "git status --porcelain | grep -q . && echo 'True'"
-local_uncommitted = subprocess.run(uncommited_command, cwd=source, shell=True, text=True, capture_output=True).stdout
 
-if local_uncommitted:
-    print(local_uncommitted.strip())
-else :
-    print("False")
+def local_uncommitted(source):
+    """
+    Return True if there is uncommitted local files.
+    Return False if there is no uncommitted local files.
+    """
+    uncommited_command = "git status --porcelain | grep -q . && echo 'True'"
+    response =  subprocess.run(uncommited_command, cwd=source, shell=True, text=True, capture_output=True).stdout
+    if response.strip() == "True":
+        return True
+    else:
+        return False
 
+print(local_uncommitted(source))

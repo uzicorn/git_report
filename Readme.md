49ebef9f178cdf3833ee815d485aa27014795124
uzicorn <zenini.salim@gmail.com>
2025-11-28 14:04:52 +0100

    final commit

--- Readme.md
+++ Readme.md
@@ -1,16 +1,18 @@
-# TEST GIT REPO
+# GIT STATUS REPORT
+
+## Context
+I have a lot of ongoing git projects, some of them are collaborations with other devs, some others are personal markdown notes i store on github, usually taken on my phone when i don't have my laptop. Last case is when i dev in an other machine.
 
 ## Goal 
-- To dev a local script that runs inside my project code repository.
+- Dev a local script that runs inside my project code repository.
 - Each sub-directory is a git repo. The script output must be a report on the versioning status between **local** and **remote**.
+
 ``` bash
-/home/uzicorn/code/is_git:
-    ├── acopole
-    ├── bank_account_analysis
-    ├── books-memento
-    ├── cvtheque
-    ├── dispatcher
-    └── (...)
+root_dir:
+├── project_1
+├── project_2
+├── project_2
+└── (...)
 
 ```
 ## Covered use cases
@@ -24,7 +26,7 @@
 | `diverged`            | None                      | TRUE                | TRUE                  |
 
 ## Possible statuses per repo 
-Each repo can have up to 2 statuses 
+Each repo can have 1 up to 2 statuses : 
 - in_sync
 - local_uncommitted
 - local_uncommitted + [local_behind, local_ahead, diverged]
@@ -32,5 +34,25 @@ Each repo can have up to 2 statuses
 - local_ahead
 - diverged
 
+## Usage
+```bash
+    py -m git_report --help
+    py -m git_report.git_report $(root_dir)
+    py -m git_report.git_report $(root_dir) --filter_dir "dir_1, ... ,dir_n"
+```
+
 ## Git commands
 
+**local branch ahead or behind relative to local**
+```bash
+git rev-list --count origin/main..HEAD  # Count the branches ahead  of local 
+                     HEAD..origin/main  # Count the branches behind of local 
+```
+
+**Uncommitted changes in local branch**
+```bash
+git status --porcelain | grep -q . && echo 'True' # Returns True if one+ file is changed
+```
+
+## Licence 
+No licence
--- git_report.py
+++ git_report.py
@@ -1,54 +1,94 @@
+status_meaning ="""
+| Status                | Local Uncommitted Changes | Local Commits Ahead | Local Commits Behind  |
+|-----------------------|---------------------------|---------------------|-----------------------|
+| `in_sync`             | FALSE                     | FALSE               | FALSE                 |
+| `local_uncommitted`   | TRUE                      | None                | None                  |
+| `local_behind`        | None                      | FALSE               | TRUE                  |
+| `local_ahead`         | None                      | TRUE                | FALSE                 |
+| `diverged`            | None                      | TRUE                | TRUE                  |
+"""
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from os import path, listdir
 import subprocess
-import os
+import pandas
 
-source = "/home/uzicorn/code/is_git/test"
-data  = {"local_uncommitted": None, "local_ahead": None, "local_behind": None, "status": []}
 
-def local_uncommitted(source):
+def local_uncommitted(dir_path):
     """
     Return True if there is uncommitted local files.
     Return False if there is no uncommitted local files.
     """
     uncommited_command = "git status --porcelain | grep -q . && echo 'True'"
-    response =  subprocess.run(uncommited_command, cwd=source, shell=True, text=True, capture_output=True)
+    response =  subprocess.run(uncommited_command, cwd=dir_path, shell=True, text=True, capture_output=True)
     
     return bool(response.stdout.strip())
 
 
-def local_ahead_behind(source):
+def local_ahead_behind(dir_path):
     """
     Return integer between [0 , n] {n>0, n = number of commits ahead}
     """
-    subprocess.run("git fetch", shell=True, cwd=source, capture_output=True)
+    subprocess.run("git fetch", shell=True, cwd=dir_path, capture_output=True)
     local_ahead_command  = "git rev-list --count origin/main..HEAD"
     local_behind_command = "git rev-list --count HEAD..origin/main"
-    count_ahead  = int(subprocess.run(local_ahead_command,  cwd=source, shell=True, capture_output=True, text=True).stdout)
-    count_behind = int(subprocess.run(local_behind_command, cwd=source, shell=True, capture_output=True, text=True).stdout)
-
+    count_ahead  = int(subprocess.run(local_ahead_command,  cwd=dir_path, shell=True, capture_output=True, text=True).stdout)
+    count_behind = int(subprocess.run(local_behind_command, cwd=dir_path, shell=True, capture_output=True, text=True).stdout)
     return count_ahead, count_behind
 
-count_ahead, count_behind = local_ahead_behind(source)
-is_uncommitted =  local_uncommitted(source)
+def dir_git_status(dir_path, dir_name):
+    data  = {"repo": dir_name}
+    
+    count_ahead, count_behind = local_ahead_behind(dir_path)
+    is_uncommitted =  local_uncommitted(dir_path)
 
-data["local_uncommitted"] = is_uncommitted
-data["local_ahead"] = count_ahead > 0
-data["local_behind"] = count_behind > 0
+    status = []
+    if not is_uncommitted and count_ahead == 0 and count_behind == 0:
+        status.append("in_sync")
+    if is_uncommitted:
+        status.append("local_uncommited")
+    if count_ahead == 0 and count_behind > 0:
+        status.append("local_behind")
+    if count_behind == 0 and count_ahead > 0:
+        status.append("local_ahead")
+    if count_ahead > 0 and count_behind > 0:
+        status.append("diverged")
+    
+    data["local_uncommitted"] = is_uncommitted
+    data["local_ahead"] = count_ahead > 0
+    data["local_behind"] = count_behind > 0
+    data["status"] = status
+    
+    return data
 
-if not is_uncommitted and count_ahead == 0 and count_behind == 0:
-    data["status"].append("in_sync")
+def report (root_dir, filter_dir=[]):
+    report = []
+    
+    list_dir_name = listdir(root_dir)
+    
+    if filter_dir:
+        list_dir_name = [dir_name for dir_name in list_dir_name if dir_name in filter_dir]
 
-if is_uncommitted:
-    data["status"].append("local_uncommited")
+    for dir_name in list_dir_name:
+        dir_path = path.join(root_dir, dir_name)
+        report.append(dir_git_status(dir_path, dir_name))
+    
+    return pandas.DataFrame(report).to_markdown()
 
-if count_ahead == 0 and count_behind > 0:
-    data["status"].append("local_behind")
+if __name__ == '__main__':
+    parser = ArgumentParser(
+        description= f"Git repository status report : {status_meaning}",
+        formatter_class= RawDescriptionHelpFormatter
+    )
+    parser.add_argument("root_dir", 
+                        help="The root_dir where the git repos are stored")
+    
+    parser.add_argument("--filter_dir", 
+                        required=False,
+                        metavar="DIRS",
+                        help="""Filter report to specific directories --filer_dir "dir_1, ..., dir_n" """)
 
-if count_behind == 0 and count_ahead > 0:
-    data["status"].append("local_ahead")
-
-if count_ahead > 0 and count_behind > 0:
-    data["status"].append("diverged")
-
-
-
-print(data)
+    args = parser.parse_args()
+    
+    print(
+        report(args.root_dir, args.filter_dir)
+          )
